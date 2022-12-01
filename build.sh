2019632504fec1cbce257f7ac721989118a8e205
Linux User <34406967+LinuxUserGD@users.noreply.github.com>
2022-12-01 19:52:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,34 +16,24 @@ target=/opt/nuitka-python${short_version}
 # Allow to overload the compiler used via CC environment variable
 if [ "$CC" = "" ]
 then
-  CC=gcc
-  CXX=g++
+  CC=clang
+  CXX=clang++
 else
   CXX=`echo "$CC" | sed -e 's#cc#++#'`
 fi
 
-export CC
-export CXX
-
 # The UCS4 has best compatibility with wheels on PyPI it seems.
 ./configure --prefix=$target --disable-shared --enable-ipv6 --enable-unicode=ucs4 \
-  --enable-optimizations --with-lto --without-gcc --with-computed-gotos --with-fpectl \
+  --enable-optimizations --with-lto --without-gcc --without-icc --with-clang --with-computed-gotos --with-fpectl \
   CC=clang \
   CXX=clang++ \
-  CFLAGS="-g" \
-  LDFLAGS="-g -Xlinker -export-dynamic -rdynamic -Bsymbolic-functions -Wl,-z,relro" \
   LIBS="-lffi -lbz2 -luuid -lsqlite3 -llzma"
 
-make -j 4 \
-        EXTRA_CFLAGS="-flto -fuse-linker-plugin -fno-fat-lto-objects" \
-        PROFILE_TASK='./Lib/test/regrtest.py -x test_bsddb3 test_compiler test_cpickle test_cprofile test_dbm_dumb test_dbm_ndbm test_distutils test_ensurepip test_gdb test_io test_linuxaudiodev test_multiprocessing test_ossaudiodev test_platform test_pydoc test_socketserver test_subprocess test_sundry test_thread test_threaded_import test_threadedtempfile test_threading test_threading_local test_threadsignals test_xmlrpc test_zipfile' \
-        profile-opt
-
-make build_all_merge_profile
+make -j 4
 
 # Delayed deletion of old installation, to avoid having it not there for testing purposes
 # while compiling, which is slow due to PGO beign applied.
-sudo rm -rf $target && sudo CC=$CC CXX=$CXX make install
+sudo rm -rf $target && sudo CC=clang CXX=clang++ make install
 
 # Make sure to have pip installed, might even remove it afterwards, Debian
 # e.g. doesn't include it.
